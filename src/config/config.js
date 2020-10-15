72ed95b1a1013ac0f7c5244720611b9a54feb735
nellyganza <nishimwelys@gmail.com>
2020-10-15 10:26:18 +0200

    CHORE configure the app to use postgres database :
    Task
    ----
    - configuration of sequelize
    - configuration of postgres database

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: process.env.TEST,
+    dialect: process.env.DB_DIALECT_TEST,
+  },
+};
---
+++ src/migrations/20201015074658-create-user.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+      },
+      userName: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
