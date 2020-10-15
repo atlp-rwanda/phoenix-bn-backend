9890f71d698fe0dd9deda4cf8aa9a7af84ab5ada
nellyganza <nishimwelys@gmail.com>
2020-10-15 10:26:18 +0200

    Chore congigure app to use postgres database :

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: process.env.TEST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOSTNAME_PROD,
+    port: process.env.DB_PORT_PROD,
+    dialect: 'postgres',
+  },
+};
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    fullName: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
